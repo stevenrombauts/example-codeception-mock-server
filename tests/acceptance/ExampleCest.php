11a82bd524479cb1e28d33021b8e766f5ee3a31e
Steven Rombauts <steven@timble.net>
2018-03-10 16:15:25 +0000

    Start Mock Webserver in ExampleCest

--- tests/acceptance/ExampleCest.php
+++ tests/acceptance/ExampleCest.php
@@ -1,12 +1,21 @@
 <?php
+use donatj\MockWebServer\MockWebServer;
+
 class ExampleCest
 {
+  private static $__mock_webserver;
+
   public function _before(AcceptanceTester $I)
   {
+    $server = new MockWebServer(8001);
+    $server->start();
+
+    self::$__mock_webserver = $server;
   }
 
   public function _after(AcceptanceTester $I)
   {
+    self::$__mock_webserver->stop();
   }
 
   public function checkQuote(AcceptanceTester $I)
